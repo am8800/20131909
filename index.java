1ea4cea0b4c131c2e730c9fd66e33fac1b0f5c65
am8800 <am167930@gmail.com>
2016-12-20 13:12:11 +0900

    Revert "remove line추가"
    This reverts commit 24ab87479f28ca9631a3b49423d997179ff558e8.

--- index.java
+++ index.java
@@ -4,7 +4,6 @@ public class index {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
-		System.out.println("remove this");
 		System.out.println("Hello java");
 	}
 
