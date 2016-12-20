b51fdbb35fd7c941e6a56429eb3714f59e678682
am8800 <am167930@gmail.com>
2016-12-20 15:41:42 +0900

    visible이동 레이아웃 null 삭제

--- index.java
+++ index.java
@@ -12,23 +12,19 @@ public class index {
 		System.out.println("Hello java");
 		 
 		JFrame j = new JFrame();
-		j.setVisible(true);
 		j.setSize(50, 100);
 		
 		System.out.println("·¹ÀÌ¾Æ¿ô Ãß°¡Àü");
-		j.setLayout(null);
 		
 		mkFunc();
-		
-		
 		j.add(setButton("txt changed"));
+		j.setVisible(true);
 	}
 	static void mkFunc(){
 		JTextArea Area = new JTextArea("¾È³ç");
 	}
 	static JButton setButton(String str){
 		JButton button1 = new JButton(str);
-		
 		return button1;
 	}
 }
